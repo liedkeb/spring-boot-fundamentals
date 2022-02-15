af45dd576e6bdbc06973b6c4b746c6b380dd4ea7
Bartosz Liedke <bliedke@psi.de>
2022-02-15 15:37:19 +0100

    updated entities fields

--- src/main/java/com/pluralsight/entity/Release.java
+++ src/main/java/com/pluralsight/entity/Release.java
@@ -5,12 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
-/**
- * TODO: Describe this class (The first line - until the first dot - will interpret as the brief description).
- */
 @Entity
 public class Release {
     @Id
